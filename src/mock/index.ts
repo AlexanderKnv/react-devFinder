a10b14b8a0977b3950522e8d0ed20f8018987088
Konaschkov <konashkov23@gmail.com>
2023-10-15 11:25:28 +0200

    Add UserCard

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "AlexanderKnv",
+  "avatar": "",
+  "name": "Alexander Konaschkov",
+  "company": "Traum",
+  "blog": "alex.com",
+  "location": "Berlin",
+  "bio": "React developer",
+  "twitter": "twit",
+  "repos": 64,
+  "followers": 184,
+  "following": 25,
+  "created": "2018-02-07T16:18:45Z",
+}
